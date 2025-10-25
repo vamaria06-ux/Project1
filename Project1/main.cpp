e700011db7e58954f99089b1f818fae9e5ebed70
vamaria06-ux <mashaulanova545@gmail.com>
2025-10-25 13:09:52 +0300

    isPyth

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+bool isPyth(unsigned a, unsigned b, unsigned c);
 
 int main() {
 	using u_t = unsigned;
 	u_t a = 0;
 	size_t count = 0;
 	while (std::cin >> a) {
-		count+=a*a;
+		count+=isPyth(a,b,c)?1:0;
+		c = b;
+		b = a;
 	}
 
-	std::cin >> a;
 	if (std::cin.eof()) {
 		std::cout << count;
 		std::cout << "\n";
@@ -17,4 +19,10 @@ int main() {
 		std::cerr << "Error\n";
 		return 1;
 	}
+}
+bool isPyth(unsigned a, unsigned b, unsigned c) {
+	bool p = a * a == (b * b + c * c);
+	p = p || (b * b == (a * a + c * c));
+	p = p || (c * c == (a * a + b * b));
+	return p;
 }

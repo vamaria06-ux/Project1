b8bd7ba7f5cb4731280cd93a69c6ce7d441ae401
vamaria06-ux <mashaulanova545@gmail.com>
2025-10-25 13:09:52 +0300

    isPyth new

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,8 +3,10 @@ bool isPyth(unsigned a, unsigned b, unsigned c);
 
 int main() {
 	using u_t = unsigned;
-	u_t a = 0;
+	u_t a = 0, b =0, c = 0;
+	std::cin >> c >> b;
 	size_t count = 0;
+
 	while (std::cin >> a) {
 		count+=isPyth(a,b,c)?1:0;
 		c = b;
@@ -15,6 +17,7 @@ int main() {
 		std::cout << count;
 		std::cout << "\n";
 	}
+
 	else if (std::cin.fail()) {
 		std::cerr << "Error\n";
 		return 1;
